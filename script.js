5d636185d78d78081b85afcb5f7bd2f01f3dde15
dmitriyg0r <dmitriyg0r@yandex.ru>
2024-07-27 13:59:23 +0300

    update

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const progressBar = document.getElementById('progressBar');
 const loadingElement = document.getElementById('loading');
 const resultElement = document.getElementById('result');
 const compressedSizeElement = document.getElementById('compressedSize');
+const originalsize = document.getElementById('originalsize');
 const downloadBtn = document.getElementById('downloadBtn');
 const newimage = document.getElementById('newimage');
 
@@ -56,6 +57,7 @@ compressBtn.addEventListener('click', async () => {
     loadingElement.style.display = 'inline-block';
     resultElement.innerHTML = '';
     compressedSizeElement.textContent = '';
+    originalSizeElement.textContent = '';
     downloadBtn.style.display = 'none';
     newimage.style.display = 'none';
 
@@ -67,6 +69,7 @@ compressBtn.addEventListener('click', async () => {
                 const img = document.createElement('img');
                 img.src = event.target.result;
                 resultElement.appendChild(img);
+                originalSizeElement.textContent = `размер до сжатия: ${(file.size / 1024 / 1024).toFixed(2)} MB`;
                 compressedSizeElement.textContent = `Сжатый размер: ${(compressedFile.size / 1024 / 1024).toFixed(2)} MB`;
                 newimage.style.display = 'inline';
                 newimage.onclick = () => {
