a918473ee4e93f4fde4a00b7bcc53a0a18ee0691
dmitriyg0r <dmitriyg0r@yandex.ru>
2024-07-27 13:38:56 +0300

    updatejs

--- script.js
+++ script.js
@@ -1,88 +1,93 @@
-document.getElementById('fileInput').addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    const compressBtn = document.getElementById('compressBtn');
-    const originalSizeElement = document.getElementById('originalSize');
-    const preview = document.getElementById('preview');
+      document.getElementById('fileInput').addEventListener('change', (event) => {
+        const files = event.target.files;
+        const compressBtn = document.getElementById('compressBtn');
+        const originalSizeElement = document.getElementById('originalSize');
+        const preview = document.getElementById('preview');
 
-    if (file) {
-        originalSizeElement.textContent = `Исходный размер: ${(file.size / 1024 / 1024).toFixed(2)} MB`;
-        compressBtn.style.display = 'inline';
+        if (files.length > 0) {
+            originalSizeElement.textContent = `Выбрано изображений: ${files.length}`;
+            compressBtn.style.display = 'inline';
 
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const img = document.createElement('img');
-            img.src = e.target.result;
             preview.innerHTML = '';
-            preview.appendChild(img);
-        };
-        reader.readAsDataURL(file);
-    } else {
-        originalSizeElement.textContent = '';
-        compressBtn.style.display = 'none';
-        preview.innerHTML = '';
-    }
-});
-
-document.getElementById('compressBtn').addEventListener('click', async () => {
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-
-    if (!file) {
-        alert('Please select an image file');
-        return;
-    }
-
-    const options = {
-        maxSizeMB: 1, // Максимальный размер в мегабайтах
-        maxWidthOrHeight: 1024, // Максимальная ширина или высота
-        useWebWorker: true,
-        onProgress: (progress) => {
-            const progressBar = document.getElementById('progress');
-            progressBar.style.width = `${progress}%`;
-            progressBar.textContent = `${progress}%`;
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const img = document.createElement('img');
+                    img.src = e.target.result;
+                    preview.appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            }
+        } else {
+            originalSizeElement.textContent = '';
+            compressBtn.style.display = 'none';
+            preview.innerHTML = '';
         }
-    };
+    });
 
-    // Показать прогресс-бар и скрыть кнопку сжатия
-    const progressBar = document.getElementById('progressBar');
-    progressBar.style.display = 'block';
-    document.getElementById('compressBtn').style.display = 'none';
+    document.getElementById('compressBtn').addEventListener('click', async () => {
+        const fileInput = document.getElementById('fileInput');
+        const files = fileInput.files;
 
-    // Показать загрузочную анимацию
-    const loadingElement = document.getElementById('loading');
-    loadingElement.style.display = 'inline-block';
-    document.getElementById('result').innerHTML = '';
-    document.getElementById('compressedSize').textContent = '';
-    document.getElementById('downloadBtn').style.display = 'none';
+        if (files.length === 0) {
+            alert('Please select image files');
+            return;
+        }
 
-    try {
-        const compressedFile = await imageCompression(file, options);
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            const img = document.createElement('img');
-            img.src = event.target.result;
-            document.getElementById('result').innerHTML = '';
-            document.getElementById('result').appendChild(img);
-            document.getElementById('compressedSize').textContent = `Сжатый размер: ${(compressedFile.size / 1024 / 1024).toFixed(2)} MB`;
-            document.getElementById('newimage').style.display = 'inline';
-            document.getElementById('newimage').onclick = () => {
-                location.reload();
-            };
-            document.getElementById('downloadBtn').style.display = 'inline';
-            document.getElementById('downloadBtn').onclick = () => {
-                const a = document.createElement('a');
-                a.href = img.src;
-                a.download = 'compressed_image.jpg';
-                a.click();
-            };
+        const options = {
+            maxSizeMB: 1, // Максимальный размер в мегабайтах
+            maxWidthOrHeight: 1024, // Максимальная ширина или высота
+            useWebWorker: true,
+            onProgress: (progress) => {
+                const progressBar = document.getElementById('progress');
+                progressBar.style.width = `${progress}%`;
+                progressBar.textContent = `${progress}%`;
+            }
         };
-        reader.readAsDataURL(compressedFile);
-    } catch (error) {
-        console.error('Error compressing image:', error);
-    } finally {
-        // Скрыть загрузочную анимацию и прогресс-бар
-        loadingElement.style.display = 'none';
-        progressBar.style.display = 'none';
-        document.getElementById('compressBtn').style.display = 'inline';
-    }
-});
+
+        // Показать прогресс-бар и скрыть кнопку сжатия
+        const progressBar = document.getElementById('progressBar');
+        progressBar.style.display = 'block';
+        document.getElementById('compressBtn').style.display = 'none';
+
+        // Показать загрузочную анимацию
+        const loadingElement = document.getElementById('loading');
+        loadingElement.style.display = 'inline-block';
+        document.getElementById('result').innerHTML = '';
+        document.getElementById('compressedSize').textContent = '';
+        document.getElementById('downloadBtn').style.display = 'none';
+
+        try {
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                const compressedFile = await imageCompression(file, options);
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    const img = document.createElement('img');
+                    img.src = event.target.result;
+                    document.getElementById('result').appendChild(img);
+                    document.getElementById('compressedSize').textContent = `Сжатый размер: ${(compressedFile.size / 1024 / 1024).toFixed(2)} MB`;
+                    document.getElementById('newimage').style.display = 'inline';
+                    document.getElementById('newimage').onclick = () => {
+                        location.reload();
+                    };
+                    document.getElementById('downloadBtn').style.display = 'inline';
+                    document.getElementById('downloadBtn').onclick = () => {
+                        const a = document.createElement('a');
+                        a.href = img.src;
+                        a.download = 'compressed_image.jpg';
+                        a.click();
+                    };
+                };
+                reader.readAsDataURL(compressedFile);
+            }
+        } catch (error) {
+            console.error('Error compressing image:', error);
+        } finally {
+            // Скрыть загрузочную анимацию и прогресс-бар
+            loadingElement.style.display = 'none';
+            progressBar.style.display = 'none';
+            document.getElementById('compressBtn').style.display = 'inline';
+        }
+    });
